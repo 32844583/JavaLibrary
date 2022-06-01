c34a1e1a9ce274eea1768e2c535a97e3601d23f0
32844583 <youto201255@gmail.com>
2022-06-01 21:43:47 +0800

    Create userbook.java

---
+++ teamwork/userbook.java
@@ -0,0 +1,39 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+public class userbook{
+	private String name;
+	private String borrower;
+	private String publish;
+	private String author;
+	private String borrow_date;
+	private String retrieve_date;
+	public userbook(String n, String p, String a, String bor){
+		this.name = n;
+		this.publish = p;
+		this.author = a;
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		
+		Date borrow = new Date();
+		Date retrieve;
+		Calendar c = Calendar.getInstance();
+		c.setTime(borrow);
+		c.add(Calendar.DATE, 10);
+		retrieve = c.getTime();
+		this.retrieve_date = dateFormat.format(retrieve);
+		this.borrow_date = dateFormat.format(borrow);
+		this.borrower = bor;
+	}
+	public userbook(String n){
+		this.name = n;
+	}
+	public String getname(){return name;}
+	public String getpublish(){return publish;}
+	public String getauthor(){return author;}
+	public String getretrievedate(){return retrieve_date;}
+	public String getborrower(){return borrower;}
+	public void setname(String n){this.name = n;}
+	public void setpublish(String p){this.publish = p;}
+	public void setauthor(String a){this.author = a;}
+	
+}
