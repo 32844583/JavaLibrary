44d1f0849160a769d14a14d74d0a133357890f24
32844583 <youto201255@gmail.com>
2022-06-04 00:46:25 +0800

    Update userbook.java

--- teamwork/userbook.java
+++ teamwork/userbook.java
@@ -3,15 +3,17 @@ import java.util.Calendar;
 import java.util.Date;
 public class userbook{
 	private String name;
-	private String borrower;
 	private String publish;
 	private String author;
 	private String borrow_date;
 	private String retrieve_date;
-	public userbook(String n, String p, String a, String bor){
-		this.name = n;
-		this.publish = p;
-		this.author = a;
+	private String situation;
+	//­É¾\
+	public userbook(String n, String p, String a){
+		setname(n);
+		setpublish(p);
+		setauthor(a);
+
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		
 		Date borrow = new Date();
@@ -22,8 +24,16 @@ public class userbook{
 		retrieve = c.getTime();
 		this.retrieve_date = dateFormat.format(retrieve);
 		this.borrow_date = dateFormat.format(borrow);
-		this.borrower = bor;
+		
 	}
+	//¹w¬ù
+	public userbook(String n, String p, String a, String s){
+		setname(n);
+		setpublish(p);
+		setauthor(a);
+		setsituation(s);
+	}
+	
 	public userbook(String n){
 		this.name = n;
 	}
@@ -31,9 +41,9 @@ public class userbook{
 	public String getpublish(){return publish;}
 	public String getauthor(){return author;}
 	public String getretrievedate(){return retrieve_date;}
-	public String getborrower(){return borrower;}
+	public String getsituation(){return situation;}
 	public void setname(String n){this.name = n;}
 	public void setpublish(String p){this.publish = p;}
 	public void setauthor(String a){this.author = a;}
-	
+	public void setsituation(String s){this.situation = s;}
 }
