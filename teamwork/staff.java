8703b690b870ff80f93530566a3165f30a9ad59a
wei <jerrychen99999#gmail.com>
2022-06-04 19:26:35 +0800

    new version

--- teamwork/staff.java
+++
@@ -1,7 +0,0 @@
-public class staff extends user{
-	private int lendup;
-	public staff (String n, String acc, String pass, String ident){
-		super(n, acc, pass, ident);
-		this.lendup = 10;
-	}
-}
--- teamwork/teacher.java
+++
@@ -1,7 +0,0 @@
-public class teacher extends user{
-	private int lendup;
-	public teacher (String n, String acc, String pass, String ident){
-		super(n, acc, pass, ident);
-		this.lendup = 10;
-	}
-}
