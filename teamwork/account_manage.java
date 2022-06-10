182d557350a25c34d776329cae8fa82a83154161
32844583 <youto201255@gmail.com>
2022-06-11 00:10:43 +0800

    Update account_manage.java

--- teamwork/account_manage.java
+++ teamwork/account_manage.java
@@ -3,25 +3,60 @@ import javax.swing.*;
 import java.awt.*;
 public class account_manage{
 	public static ArrayList<user> users  = new ArrayList<user>();
-	public void add(){
-		regis  re = new regis();
-		String information;
-		int check = 0;
-		information = re.gui();
-		String[] info_part = information.split("-");
-		// System.out.print(u.getname() + u.getaccount() + u.getpassword() + u.getidentification());
-		if(info_part.length == 4){
-			for(user u : users){
-				if(u.getname().equals(info_part[3])){check = 1;}
-			}
-			if(check == 0){users.add(new user(info_part [3], info_part [0], info_part [1], info_part[2]));}
-			else{
-				JOptionPane.showMessageDialog(null,"該帳號已存在");
-				add();
-			}
-		}
-		
-	}
+   public void regis() {
+	      JTextField xField = new JTextField(5);
+	      JTextField yField = new JTextField(5);
+	      JTextField uField = new JTextField(5);
+	      JFrame jf = new JFrame();
+	      jf.setSize(250,250);
+	      jf.setLocationRelativeTo(null);
+	      JPanel myPanel = new JPanel();
+	      GridLayout experimentLayout = new GridLayout(0,3);
+	      myPanel.setLayout(experimentLayout);
+	      myPanel.add(new JLabel("姓名:")); 
+	      myPanel.add(uField); 
+	      myPanel.add(Box.createHorizontalStrut(15));// a spacer
+	      myPanel.add(new JLabel("帳號:"));
+	      myPanel.add(xField);
+	      myPanel.add(Box.createHorizontalStrut(15)); // a spacer
+	      myPanel.add(new JLabel("密碼:"));
+	      myPanel.add(yField);
+	      myPanel.add(Box.createHorizontalStrut(15));
+	      JLabel label = new JLabel("身份別:");
+	      myPanel.add(label);
+	      String[] listData = {"Student", "Teacher", "Staff", "Admin"};
+	      final JComboBox<String> comboBox = new JComboBox<String>(listData);
+	      comboBox.setSelectedIndex(0);
+	      myPanel.add(comboBox);
+	      int result = JOptionPane.showConfirmDialog(null, myPanel,"registration", JOptionPane.OK_CANCEL_OPTION);
+	      if (result == JOptionPane.OK_OPTION) {
+	    	  if(xField.getText().equals("") || uField.getText().equals("") || yField.getText().equals("") ) {
+	    		  JOptionPane.showMessageDialog(null,"有尚未填入資料");
+	    	  }
+	    	  else {
+	    		  int check = 0;
+		    	  for(user u: users) {
+		    		  if(u.getname().equals(uField.getText()) || u.getaccount().equals(xField.getText())) {
+		    			  check = 1;
+		    		  }
+		    	  }
+		    	  if(check == 1) {
+		    		  JOptionPane.showMessageDialog(null,"帳號重複註冊");
+		    	  }
+		    	  else {
+		    		  JOptionPane.showMessageDialog(null,"註冊成功！\n歡迎: "+comboBox.getSelectedItem()+" "+uField.getText());
+		        	  if(comboBox.getSelectedItem().toString() == "Student") {users.add(new student(uField.getText(), xField.getText(), yField.getText(), comboBox.getSelectedItem().toString()));}
+		        	  else if(comboBox.getSelectedItem().toString() == "Teacher") {users.add(new teacher(uField.getText(), xField.getText(), yField.getText(), comboBox.getSelectedItem().toString()));}
+		        	  else if(comboBox.getSelectedItem().toString() == "Staff") {users.add(new staff(uField.getText(), xField.getText(), yField.getText(), comboBox.getSelectedItem().toString()));}
+		        	  else if(comboBox.getSelectedItem().toString() == "Admin") {users.add(new adminstrator(uField.getText(), xField.getText(), yField.getText(), comboBox.getSelectedItem().toString()));}
+		    	  }
+	    		  
+	    	  }
+	      }
+	      else {
+	    	  JOptionPane.showMessageDialog(null,"取消註冊");
+	      }
+	   }
 	public user login(){
       		JTextField xField = new JTextField(5);
       		JTextField yField = new JTextField(5);
@@ -35,26 +70,27 @@ public class account_manage{
       		myPanel.add(yField);
       		String login_button[] = {"登入", "取消"};
       		int result = JOptionPane.showOptionDialog(null, myPanel,"登入介面", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, login_button,null);
-		int check = 0;
-		int count = 0;
-		user ex = new user("", "", "", "");
-		while(result == 0 && (check == 0 && count < 3)){
-			for(user u:users){
-				if (u.getaccount().equals(xField.getText()) && u.getpassword().equals(yField.getText())){
-					JOptionPane.showMessageDialog(null,"登入成功!");
-					check = 1;
-					return u;
-				}
-						
-                    		}
-			if (check == 0){
-				JOptionPane.showMessageDialog(null,"登入失敗, 請確認帳號密碼是否正確");
-				count +=1;
-				result = JOptionPane.showConfirmDialog(null, myPanel,"login", JOptionPane.OK_CANCEL_OPTION);
-			}
+      		int check = 0;
+      		int count = 0;
+      		student ex = new student("", "", "", "");
+      		while(result == 0 && (check == 0 && count < 3)){
+      			for(user u:users){
+      				if (u.getaccount().equals(xField.getText()) && u.getpassword().equals(yField.getText())){
+						JOptionPane.showMessageDialog(null,"登入成功!");
+						check = 1;
+						return u;
+      				}		
+      			}
+				if (check == 0){
+					JOptionPane.showMessageDialog(null,"登入失敗, 請確認帳號密碼是否正確");
+					count +=1;
+					result = JOptionPane.showConfirmDialog(null, myPanel,"login", JOptionPane.OK_CANCEL_OPTION);
+				
      		}
 		if(result == JOptionPane.CANCEL_OPTION){JOptionPane.showMessageDialog(null,"取消登入");}
-		return ex;  // 帳號密碼錯誤或取消登入
+      	}
+      	return ex;  // 帳號密碼錯誤或取消登入
+      	
 	}
 	public void watch_user(user admin){
 		JFrame jf = new JFrame();
@@ -114,10 +150,9 @@ public class account_manage{
 		users.removeIf(user -> user.getname() == delete_name);
 	}
 	public void initialize_user(){
-		users.add(new user("王曉明", "user1", "user1", "Student"));
-		users.add(new user("王大明", "user3", "user3", "Student"));
-		users.add(new user("李小明", "user2", "user2", "Admin"));
+		users.add(new student("王曉明", "user1", "user1", "Student"));
+		users.add(new student("王大明", "user3", "user3", "Student"));
+		users.add(new adminstrator("李小明", "user2", "user2", "Admin"));
 	}
-	
 	
 }
