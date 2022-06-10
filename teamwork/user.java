111a8c88c4299be7d2aeeec92ba706f29d035aba
32844583 <youto201255@gmail.com>
2022-06-11 00:11:06 +0800

    Update user.java

--- teamwork/user.java
+++ teamwork/user.java
@@ -1,21 +1,22 @@
-public class user{
+public abstract class user{
 	private String name;
 	private String account;
 	private String password;
 	private String identification;
 	public user(String n, String acc, String pass, String ident){
-		this.name = n;
-		this.account = acc;
-		this.password = pass;
-		this.identification = ident;
+		setname(n);
+		setaccount(acc);
+		setpassword(pass);
+		setidentification(ident);
 	}
-	public book searchbook(){
-		book_manage b = new book_manage();
-		return b.sea();
-	}
-	public void setidentification(String ident) {this.identification = ident;}
+
+
 	public String getname(){return name;}
 	public String getaccount(){return account;}
 	public String getpassword(){return password;}
 	public String getidentification(){return identification;}
+	public void setname(String n){this.name = n;}
+	public void setaccount(String acc){this.account = acc;}
+	public void setpassword(String pass){this.password = pass;}
+	public void setidentification(String ident) {this.identification = ident;}
 }
