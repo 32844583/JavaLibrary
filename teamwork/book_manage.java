4db6bf3f80b6b64987c68dd8d0894537ccb08086
32844583 <youto201255@gmail.com>
2022-06-11 00:10:53 +0800

    Update book_manage.java

--- teamwork/book_manage.java
+++ teamwork/book_manage.java
@@ -1,18 +1,9 @@
-
 import java.util.*;
 import java.awt.*;
 import javax.swing.*;
 public class book_manage{
 	static ArrayList<book> books  = new ArrayList<book>();
 	private ArrayList <book> allbook = new ArrayList <book>();
-	public void add(){
-		addbook a = new addbook();
-		String information;
-		information = a.gui();
-		String[] info_part = information.split("-");
-		// System.out.print(a.getname() + getpublish() + getauthor());
-		books.add(new book(info_part [0], info_part [2], info_part [1]));
-	}
 	public void remove(book b){
 		books.removeIf(book -> book.getname() == b.getname());
 	}
@@ -26,12 +17,14 @@ public class book_manage{
 	public void initialize_book(){
 		books.add(new book("book1", "book1", "book1"));
 		books.add(new book("book2", "book2", "book2"));
+		books.add(new book("book3", "book3", "book3"));		
 	}
 	public book borrow_book_search(String bkname){
 		book bk = new book("");
 		for(book b : books){
-			if(b.getname() == bkname){
+			if(b.getname().equals(bkname)){
 				bk = b;
+				System.out.println("find");
 			}
 		}
 		
@@ -42,13 +35,13 @@ public class book_manage{
 	JPanel myPanel = new JPanel();
 	GridLayout experimentLayout = new GridLayout(0,3);
 	myPanel.setLayout(experimentLayout);
-	myPanel.add(new JLabel("關鍵字搜尋:"));
+	myPanel.add(new JLabel("ÃöÁä¦r·j´M:"));
 	myPanel.add(xField);
 
 	int result = JOptionPane.showConfirmDialog(null, myPanel,"Search", JOptionPane.OK_CANCEL_OPTION);
 	System.out.println(result);
 	if(result == 2) {
-		JOptionPane.showMessageDialog(null, "取消搜尋");
+		JOptionPane.showMessageDialog(null, "¨ú®ø·j´M");
 	}
 	else {
 		for(book b : books){
@@ -59,4 +52,48 @@ public class book_manage{
 	}
 	return allbook;
 	}
+	public String top3(){
+		TreeMap<Integer, String> ht = new TreeMap<Integer, String>();
+		for(book bk:books){
+			ht.put(bk.getcount(), bk.getname());
+			System.out.println("success");
+		}
+		String s = "";
+		Set<Integer> keySet = ht.descendingKeySet();
+		for (Integer key : keySet) {
+            // Print key:value of the TreeMap
+            s += ht.get(key)+",";
+        }
+		System.out.println("the sequence is" + s);
+		return s; 
+		
+	}
+	public void show(){
+		for(book b : books){
+			System.out.println(b.getname());
+		}
+	}
+   public void add() {
+	      JTextField xField = new JTextField(5);
+	      JTextField yField = new JTextField(5);
+	      JTextField zField = new JTextField(5);
+	 
+	      JPanel myPanel = new JPanel();
+	      GridLayout experimentLayout = new GridLayout(0,3);
+	      myPanel.setLayout(experimentLayout);
+	      myPanel.add(new JLabel("書名："));
+	      myPanel.add(xField);
+	      myPanel.add(Box.createHorizontalStrut(15)); // a spacer
+	      myPanel.add(new JLabel("作者："));
+	      myPanel.add(yField);
+	      myPanel.add(Box.createHorizontalStrut(15)); // a spacer
+	      myPanel.add(new JLabel("出版社："));
+	      myPanel.add(zField);
+
+	      String add_button[] = {"確認增加", "取消"};
+	      int result = JOptionPane.showOptionDialog(null, myPanel,"增加書籍", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, add_button,null);
+	      if (result == JOptionPane.OK_OPTION) {
+	    	  books.add(new book(xField.getText(), yField.getText(), zField.getText()));
+	      }
+	   }
 }
