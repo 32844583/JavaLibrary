3650b6bce69a155ff84b401c1377a1d1d9cb2e5c
32844583 <youto201255@gmail.com>
2022-06-11 00:10:48 +0800

    Update adminstrator.java

--- teamwork/adminstrator.java
+++ teamwork/adminstrator.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 
 public class adminstrator extends user{
 	static ArrayList <book> search_books = new ArrayList <book>();
-
+	private String name;
+	private String account;
+	private String password;
+	private String identification;
 	public adminstrator(String n, String acc, String pass, String ident){
 		super(n, acc, pass, ident);
 	}
@@ -56,47 +59,46 @@ public class adminstrator extends user{
 				myPanel.add(new JLabel("書名:" + b.getname()));
 				myPanel.add(Box.createHorizontalStrut(15));
 			}
-		final JComboBox<String> comboBox = new JComboBox<String>();
-		JLabel label = new JLabel("請選擇要編輯的書籍");
-		myPanel.add(label);
-		
-		
-		for(book b : search_books){
-		 comboBox.addItem(b.getname());
-		}
-		
-		comboBox.setSelectedIndex(0);
-		myPanel.add(comboBox);
-		
-		int result = JOptionPane.showConfirmDialog(null, myPanel,"search",  JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
-		adbook = bm.borrow_book_search(comboBox.getSelectedItem().toString());
-		String login[] = {"編輯","刪除","驗證"};
-		int select = JOptionPane.showOptionDialog(null,"請問您要","bookmanage", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null,login,null);
-		
-		if (select !=3){
-			switch (select){
-         			case 0:
-            			revise(adbook);
-            			if (adbook.getname() == "no this book"){
-            				JOptionPane.showMessageDialog(null, "there is not this book");
-            			}
-            			break;
-         			case 1:
-         				if (adbook.getname() == "no this book"){
-         					JOptionPane.showMessageDialog(null, "there is not this book");
-         				}
-            			bm.remove(adbook);
-						JOptionPane.showMessageDialog(null, "刪除成功!");
-            			break;
-         			case 2:
-						bm.show();
-						break;
-   			}
+			final JComboBox<String> comboBox = new JComboBox<String>();
+			JLabel label = new JLabel("請選擇要編輯的書籍");
+			myPanel.add(label);
 			
-			action(u);
+			
+			for(book b : search_books){
+			 comboBox.addItem(b.getname());
+			}
+			
+			comboBox.setSelectedIndex(0);
+			myPanel.add(comboBox);
+			
+			int result = JOptionPane.showConfirmDialog(null, myPanel,"search",  JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+			if(result == 0) {
+				adbook = bm.borrow_book_search(comboBox.getSelectedItem().toString());
+				String login[] = {"編輯","刪除","驗證"};
+				int select = JOptionPane.showOptionDialog(null,"請問您要","bookmanage", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null,login,null);
+				
+				if (select !=3){
+					switch (select){
+		         			case 0:
+		            			revise(adbook);
+		            			break;
+		         			case 1:
+		            			bm.remove(adbook);
+								JOptionPane.showMessageDialog(null, "刪除成功!");
+		            			break;
+		         			case 2:
+								bm.show();
+								break;
+		   			}
+					
+					action(u);
+				}
+			}
 		}
-		
-	}
+			
+		else {
+			JOptionPane.showMessageDialog(null, "查無此書籍");
+		}
 	}
 	public void revise(book b){
 			JTextField xField = new JTextField(b.getname());
