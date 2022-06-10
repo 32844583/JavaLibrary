85dad921dbc836a8601513db7d3668493e2f3f2e
32844583 <youto201255@gmail.com>
2022-06-11 00:10:54 +0800

    Update lib.java

--- teamwork/lib.java
+++ teamwork/lib.java
@@ -1,49 +1,57 @@
 import javax.swing.JOptionPane;
 import java.util.ArrayList;
-public class lib extends account_manage{
-	public static void main(String [] args){
+
+public class lib extends account_manage {
+	public static void main(String[] args) {
 		account_manage ac = new account_manage();
 		ac.initialize_user();
 		book_manage bm = new book_manage();
 		bm.initialize_book();
 		menu();
 	}
-	public static void menu(){
-		String login[] = {"註冊","登入","查詢用","訪客"};
-    	int select;
-       	account_manage ac = new account_manage();
-		do{
-			select = JOptionPane.showOptionDialog(null,"歡迎來到中大借書系統!", "中大借閱系統", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null,login,1);
-			if (select == 0){
-				ac.add();
+
+	public static void menu() {
+		String login[] = { "註冊", "登入", "訪客" };
+		int select;
+		account_manage ac = new account_manage();
+		do {
+			select = JOptionPane.showOptionDialog(null, "歡迎來到中大借書系統！", "中大借閱系統", JOptionPane.YES_NO_CANCEL_OPTION,
+					JOptionPane.QUESTION_MESSAGE, null, login, 1);
+			if (select == 0) {
+				ac.regis();
 			}
 
-			if(select == 1){
+			if (select == 1) {
 				user u = ac.login();
 				userbook_manage ubm = new userbook_manage();
-				if (u.getidentification()  == "Teacher"){
-					
-				}
-				else if (u.getidentification().equals("Student")){
+				if (u.getidentification() == "Teacher") {
+					teacher tea = new teacher(u.getname(), u.getaccount(), u.getpassword(), u.getidentification());
+					ubm.addtea(tea);
+					tea.action();
+
+				} else if (u.getidentification().equals("Student")) {
 					student stu = new student(u.getname(), u.getaccount(), u.getpassword(), u.getidentification());
 					ubm.addstu(stu);
 					stu.action();
-					
-				}
-				else if (u.getidentification()  == "Staff"){
-				}
-				else if (u.getidentification().equals("Admin")){
-					adminstrator temp_bkm = new adminstrator(u.getname(), u.getaccount(), u.getpassword(), u.getidentification());
+
+				} else if (u.getidentification().equals("Staff")) {
+					staff sta = new staff(u.getname(), u.getaccount(), u.getpassword(), u.getidentification());
+					ubm.addsta(sta);
+					sta.action();
+
+				} else if (u.getidentification().equals("Admin")) {
+					adminstrator temp_bkm = new adminstrator(u.getname(), u.getaccount(), u.getpassword(),
+							u.getidentification());
 					temp_bkm.action(u);
+				} else {
+					System.out.println("No Existing");
 				}
-				else{System.out.println("No Existing");}
-			}
-			else if(select == 3){
+			} else if (select == 2) {
 				visitor v = new visitor();
 				v.action();
 			}
-		}while(select ==0 || select ==1 || select ==2 || select ==3);
-		JOptionPane.showMessageDialog(null,"謝謝光臨");
-   	}
-	
+		} while (select == 0 || select == 1 || select == 2 || select == 3);
+		JOptionPane.showMessageDialog(null, "謝謝光臨");
+	}
+
 }
